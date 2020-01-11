d452d0e11703248ca4206172dd3fb96f6d05e90f
russel-yang <58188452+russel-yang@users.noreply.github.com>
2020-01-10 21:48:36 -0800

    feat: build version workflow (#4)

---
+++ README.md
@@ -0,0 +1,3 @@
+# git workflow actions
+
+- buildinfo
--- buildinfo/index.js
+++ buildinfo/index.js
@@ -1,15 +1,55 @@
-const fs = require('fs');
-const path = require('path');
 const core = require('@actions/core');
-const github = require('@actions/github');
 
-try {
-  console.log('run build info action.');
+const chooseSandbox = labels => {
+  const sandboxes = ['sandbox1', 'sandbox2', 'sandbox3'];
+  for (label of labels) {
+    if (sandboxes.includes(label)) {
+      return label;
+    }
+  }
+  if (getBranchName() === 'master') {
+    return 'T2gpWebDocker-env';
+  }
+  return '';
+};
 
-  const lables = core.getInput('lables');
-  console.log(`input labels:  ${lables}`);
+const getBranchName = () => {
+  const eventName = process.env.GITHUB_EVENT_NAME;
+  const ref = process.env.GITHUB_REF;
+  let branchName;
+  if (eventName === 'push') {
+    branchName = ref.split('/').pop();
+  } else {
+    branchName = process.env.GITHUB_HEAD_REF;
+  }
+  return branchName;
+};
 
-  console.log('build info action done.');
-} catch (error) {
-  core.setFailed(error.message);
-}
+const setBranchName = () => {
+  core.setOutput('branch_name', getBranchName());
+};
+
+const setSandbox = () => {
+  const labels = JSON.parse(core.getInput('labels'));
+  const sandbox = chooseSandbox(labels);
+  core.setOutput('environment_name', sandbox);
+};
+
+const setVersion = () =>
+  core.setOutput('version', process.env.GITHUB_SHA.substring(0, 8));
+
+const main = () => {
+  try {
+    console.log('run build info action.');
+
+    setBranchName();
+    setSandbox();
+    setVersion();
+
+    console.log('build info action done.');
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+main();
