b91d71b8b63c58209a2fb751cb533f33f0c9a757
Sean LeBlanc (T2 SF) <sean.leblanc@take2games.com>
2021-02-03 15:11:50 -0500

    fix: trim non-alphanumeric start/end of branch name

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const getBranchName = () => {
       branchName = process.env.GITHUB_HEAD_REF;
       break;
   }
-  return branchName.toLowerCase().replace(/[^a-z0-9-.]/g, '-');
+  return branchName.toLowerCase().replace(/[^a-z0-9-.]/g, '-').replace(/^[^a-z0-9]+/, '').replace(/[^a-z0-9]+$/, '');
 };
 
 const setBranchName = () => {
